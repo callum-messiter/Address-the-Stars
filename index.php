b2285742c57d3dd322e30fae5c58ff7f75ac6be5
Callum Messiter <cmessiter@hotmail.co.uk>
2016-07-17 22:29:02 +0100

    Change filename to 'index.php'

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Initiate connection to the database...
-$db = mysqli_connect('localhost', 'root', '', 'words');
+$db = mysqli_connect('localhost', 'root', '', 'stellar');
 
 // Query database of words
 $words_sql = "SELECT * FROM words";
@@ -32,7 +32,8 @@ $stars_res = mysqli_query($db, $stars_sql)or die(mysqli_error());
 // Loop through every star in the array
 while($row = mysqli_fetch_array($stars_res)){
     // Store the star name in a variable
-    $star = $row['star_name'];
+    $star    = $row['star_name'];
+    $star_id = $row['star_id'];
 
     // Set $pair as random combination from $pairs...
     $ran_num = array_rand($pairs);
@@ -44,6 +45,12 @@ while($row = mysqli_fetch_array($stars_res)){
     // Set variables 
     $word        = 'star';
     $three_words = "$word.$pair";
+
+    // Add unique three-word combination to star's row in the datbase
+    $add_sql = "INSERT INTO stars (three_words) 
+                VALUES '$three_words' 
+                WHERE 'star_id' = '$star_id' "; 
+    $add_res = mysqli_query($db, $add_sql);                    
 
     // Attach the random 3-word combination to the star 
     echo $star.'&nbsp;&nbsp;&nbsp;&nbsp;'.$three_words.'<br/><br/>';
