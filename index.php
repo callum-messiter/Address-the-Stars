1f8299ee12d8e7d5d3f8b504786d731762cc7051
Callum Messiter <cmessiter@hotmail.co.uk>
2016-07-18 20:15:55 +0100

    Change three-word identifier format from 'star..' to '..'

--- index.php
+++ index.php
@@ -8,20 +8,22 @@ $words_sql = "SELECT * FROM words";
 $words_res = mysqli_query($db, $words_sql)or die(mysqli_error());
 
 // Create array of words
-$array  = array();
+$words  = array();
 
-// Loop through each word from the database and add each to the array
+// Loop through each word from the database and add each to an array
 while($row = mysqli_fetch_array($words_res)){
-    $array[] = $row['word'];
+    $words[] = $row['word'];
 }
  
-// Create array of all possible two-word combinations, from which we will randomly select our combinations 
-$pairs = array();
-foreach ($array as $word1) {
-    foreach ($array as $word2) {
-        if ($word1 !== $word2) {
-            $pairs[] = "$word1.$word2";
-        }
+// Create array of all possible three-word combinations, from which we will randomly select our combinations 
+$triplets = array();
+foreach ($words as $word1) {
+    foreach ($words as $word2) {
+        foreach($words as $word3) {
+            if ($word1 !== $word2 && $word2 !== $word3 && $word1 !== $word3) {
+                $triplets[] = "$word1.$word2.$word3";
+            }
+        }    
     }
 }
 
@@ -31,27 +33,17 @@ $stars_res = mysqli_query($db, $stars_sql)or die(mysqli_error());
 
 // Loop through every star in the array
 while($row = mysqli_fetch_array($stars_res)){
-    // Store the star name in a variable
+    // Store the star name and star_id in variables
     $star    = $row['star_name'];
     $star_id = $row['star_id'];
 
-    // Set $pair as random combination from $pairs...
-    $ran_num = array_rand($pairs);
-    $pair    = $pairs[$ran_num];
+    // Set $three_words as a random combination from the array of possible combinations...
+    $ran_num     = array_rand($triplets);
+    $three_words = $triplets[$ran_num];
 
-    // ...and remove it, in order to prevent repating combinations
-    array_splice($pairs, $ran_num, 1);
-
-    // Set variables 
-    $word        = 'star';
-    $three_words = "$word.$pair";
-
-    // Add unique three-word combination to star's row in the datbase
-    $add_sql = "INSERT INTO stars (three_words) 
-                VALUES '$three_words' 
-                WHERE 'star_id' = '$star_id' "; 
-    $add_res = mysqli_query($db, $add_sql);                    
-
+    // ...and remove this particular combination, in order to prevent repating combinations
+    array_splice($triplets, $ran_num, 1);
+   
     // Attach the random 3-word combination to the star 
     echo $star.'&nbsp;&nbsp;&nbsp;&nbsp;'.$three_words.'<br/><br/>';
 }
